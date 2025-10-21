667ae2d3d3b549b701dfde21edd10646af329689
Dan Flettre <flettre@gmail.com>
2025-10-20 23:39:31 -0500

    init

---
+++ main.lua
@@ -0,0 +1,16 @@
+_G.love = require("love")
+
+
+function love.load()
+    -- Load the game
+    _G.number = 0
+end
+
+function love.update(dt)
+    -- Update the game
+end
+
+function love.draw()
+    -- Draw the game
+    love.graphics.print("Hello World")
+end
